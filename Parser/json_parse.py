ec28a60f38948d1f4e4b440c6fa23db50e0b4cdb
Joshua Webb <162640629+jwe0@users.noreply.github.com>
2024-03-26 23:18:30 +0000

    Add files via upload

--- Parser/json_parse.py
+++ Parser/json_parse.py
@@ -13,23 +13,31 @@ and dumps it into a json database
 
 
 for file in os.listdir("Passwords/"):
-    print(file)
+    print(file.replace(" ", ""))
     with open("Passwords/" + file) as f:
         content = f.read().splitlines()
 
         for line in content:
             try:
                 router_info = line.split("|")
-                print(router_info)
                 name = router_info[0].replace(" ", "")
                 username = router_info[1].replace(" ", "")
                 password = router_info[2].replace(" ", "")
 
+                f_name = file.split(".")[0].replace(" ", "")
+
                 database[name.lower()] = {"username" : username, "password" : password}
+
+                print(database)
+
             except:
                 continue
 
-            
+                
 
-with open("database.json", 'w') as f:
-    f.write(json.dumps(database, indent=4)) 
+        with open(f"Database/{f_name}.json", 'a') as f:
+            f.write(json.dumps(database, indent=4)) 
+            database.clear()
+
+                
+
